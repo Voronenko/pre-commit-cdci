fb4cc49394381c2d9bc9b081fc8bd52a8e5499d5
Vyacheslav Voronenko <git@voronenko.info>
2020-10-19 15:35:04 +0200

    chore(hooks) pointing to files

--- circleci_validate.sh
+++ circleci_validate.sh
@@ -7,11 +7,11 @@ then
     exit
 fi
 
-if [ -z "$GITHUB_TOKEN" ]
-then
-      echo "\$GITHUB_TOKEN is not set, required by local circleci to run check"
-      exit
-fi
+#if [ -z "$GITHUB_TOKEN" ]
+#then
+#      echo "\$GITHUB_TOKEN is not set, required by local circleci to run check"
+#      exit
+#fi
 
 if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
 	echo "CircleCI Configuration Failed Validation."
